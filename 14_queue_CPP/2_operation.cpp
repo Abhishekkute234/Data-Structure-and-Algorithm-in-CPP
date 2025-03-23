cf0ebb62d85d3210ff89d109b4a11934212d16be
Abhishekkute234 <kuteabhishek2004@gmail.com>
2025-03-23 13:46:04 +0530

    adding the queue

--- 14_queue_CPP/2_operation.cpp
+++ 14_queue_CPP/2_operation.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int main(){
+int main()
+{
     queue<int> q;
     q.push(10);
     q.push(20);
     q.push(30);
+    q.push(40);
 
-    cout<<"size of q is "<< q.size() <<endl;
+    q.push(50);
+
+    cout << "size of q is " << q.size() << endl;
     q.pop();
-    cout<<"peek elemnt is "<<q.front()<<endl;
-    if(q.empty()){
-        cout<<"Q is empty "<<endl;
-
+    cout << "peek elemnt is " << q.front() << endl;
+    if (q.empty())
+    {
+        cout << "Q is empty " << endl;
     }
 
-    else{
-        cout<<"Q is not empty "<<endl;
+    else
+    {
+        cout << "Q is not empty " << endl;
+    }
+
+    cout << "elements in the queue are\n";
+
+    while (q.size() != 0)
+    {
+        cout << q.front() << "\n";
+        q.pop();
     }
 }
